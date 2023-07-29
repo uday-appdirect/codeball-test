8aa3ef17502e52d4cc70e51c3043e55c342e6143
uday-appdirect <uday.girhepunje@appdirect.com>
2023-07-29 22:41:49 +0530

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,79 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './styles.css';
 
 const App = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+  const [text, setText] = useState([])
+  const API_KEY = 'S8FOJA98XFQ7INS3VYC5LFQE2PJ60OH1J7Q0BITZ4IC981Z9CUPPT86OTJ43XLF61JXU3AY27MZS0CDI'
+
+  const getSearchResults = async (searchQuery) => {
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: `https://app.scrapingbee.com/api/v1/store/google?search=${searchQuery}&api_key=${API_KEY}&nb_results=10`,
+      headers: {}
+    };
+
+    const response = await axios.request(config);
+    return response;
+  }
+
+  const getText = async (url) => {
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: `https://app.scrapingbee.com/api/v1/?url=${url}&render_js=false&extract_rules={"text": "body"}&api_key=${API_KEY}`,
+      headers: {}
+    };
+    const response = await axios.request(config)
+    return response;
+  }
 
   const handleSearch = async () => {
+    setIsLoading(true);
     try {
-      // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint for fetching search results
-      const response = await axios.get(`https://app.scrapingbee.com/api/v1/store/google?search=${searchQuery}&api_key=DDELHPFOWPDU6NGH6W4Q6ZZ8ALAUIN0D5D0FPZSMRY5OOIR65B0YSMERYYRRG6RHZ8K2JFFMEDM6RLFE`);
+      const response = await getSearchResults(searchQuery);
       setSearchResults(response.data);
+      let textArr = [];
+      for (let i = 0; i < 5; i++) {
+        const res = await getText(response.data.organic_results[i].url)
+        textArr.push(res.data.text)
+      }
 
-      
+      setText(textArr)
+
     } catch (error) {
       console.error('Error fetching search results:', error);
       setSearchResults([]);
     }
+    setIsLoading(false);
   };
-
+  console.log(text)
   return (
-    <div>
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        placeholder="Enter your search query"
-      />
-      <button onClick={handleSearch}>Search</button>
+    <div className="container">
+      <div className="search-bar">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Enter your search query"
+          className="search-input"
+        />
+        <button onClick={handleSearch} className="search-button">
+          {isLoading ? 'Searching' : 'Search'}
+        </button>
+      </div>
 
-      <div>
-        {searchResults.length > 0 ? (
-          searchResults.map((result, index) => (
-            <div key={index}>
-              <h3><a href={result.url}>{result.heading}</a></h3>
-              <p>{result.text}</p>
+      <div className="search-results">
+        {text.length > 0 ? (
+          text.map((result, index) => (
+            <div key={index} className="result-item">
+              <h3>{searchResults.organic_results[index].title}</h3>
+              <a href={searchResults.organic_results[index].url}>{searchResults.organic_results[index].url}</a>
+              <p>{result}</p>
             </div>
           ))
         ) : (
