b561ba480e662a56d402a1bbef9c39e045d0fd07
Uday Girhepunje <115977423+uday-appdirect@users.noreply.github.com>
2024-06-13 19:21:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,25 @@ const App = () => {
     return response;
   }
 
+  // const core = require('@actions/core');
+  // const github = require('@actions/github');
+  // const { context } = require('@actions/github')
+  // const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  // const octokit = github.getOctokit(GITHUB_TOKEN);
+   
+  // const { pull_request } = context.payload;
+   
+  // async function run() {
+  //   await octokit.rest.issues.createComment({
+  //     ...context.repo,
+  //     issue_number: pull_request.number,
+  //     body: 'Thank you for submitting a pull request! We will
+  //           try to review this as soon as we can.'
+  //   });
+  // }
+   
+  // run();
+
   const getText = async (url) => {
     let config = {
       method: 'get',
