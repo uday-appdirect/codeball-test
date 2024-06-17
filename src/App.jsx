98ebf22f088afd72356038f11df85ce2a4a06c07
Uday Girhepunje <115977423+uday-appdirect@users.noreply.github.com>
2024-06-17 16:29:02 +0530

    uncomment code

--- src/App.jsx
+++ src/App.jsx
@@ -21,28 +21,28 @@ const App = () => {
     return response;
   }
 
-  // const core = require('@actions/core');
-  // const github = require('@actions/github');
-  // const { context } = require('@actions/github')
-  // const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-  // const octokit = github.getOctokit(GITHUB_TOKEN);
+  const core = require('@actions/core');
+  const github = require('@actions/github');
+  const { context } = require('@actions/github')
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const octokit = github.getOctokit(GITHUB_TOKEN);
    
-  // const { pull_request } = context.payload;
+  const { pull_request } = context.payload;
    
-  // async function run() {
-  //   await octokit.rest.issues.createComment({
-  //     ...context.repo,
-  //     issue_number: pull_request.number,
-  //     body: 'Thank you for submitting a pull request! We will
-  //           try to review this as soon as we can.'
-  //   });
-  // }
+  async function run() {
+    await octokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: pull_request.number,
+      body: 'Thank you for submitting a pull request! We will
+            try to review this as soon as we can.'
+    });
+  }
    
-  // run();
+  run();
 
-  //
-  // testing commit
-  //
+  
+  testing commit
+  
 
   const getText = async (url) => {
     let config = {
